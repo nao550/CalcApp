f1316cb2a3df125982d24ce93515022afe222fa1
nao <nao@naonomacbook-air.local>
2019-07-20 00:53:49 +0900

    Initial Commit

---
+++ CalcApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalcApp
+//
+//  Created by nao on 2019/07/20.
+//  Copyright © 2019年 nao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
